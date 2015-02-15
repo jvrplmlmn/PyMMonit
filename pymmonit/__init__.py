43850453e79788749da8dc220c92f0b3ea245b35
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2015-02-15 18:41:30 +0100

    Create MMonit class and it's constructor.

--- pymmonit/__init__.py
+++ pymmonit/__init__.py
@@ -2,3 +2,11 @@
 
 __version__ = '0.1.0'
 __author__ = 'Javier Palomo Almena'
+
+
+class MMonit:
+
+    def __init__(self, mmonit_url, username, password):
+        self.mmonit_url = mmonit_url
+        self.username = username
+        self.password = password
