e31283ad63ef6c30853eee9a337d53b23ed6b995
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2015-02-15 18:46:17 +0100

    Login method. CSRF protection is disabled

--- pymmonit/__init__.py
+++ pymmonit/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import requests
+
 __version__ = '0.1.0'
 __author__ = 'Javier Palomo Almena'
 
@@ -10,3 +12,15 @@ class MMonit:
         self.mmonit_url = mmonit_url
         self.username = username
         self.password = password
+
+        self.login()
+
+    def login(self):
+        self.session = requests.session()
+        self.session.get(self.mmonit_url + '/index.csp')
+        login_data = {
+            "z_username": self.username,
+            "z_password": self.password,
+            "z_csrf_protection": "off"
+        }
+        self.session.post(self.mmonit_url + '/z_security_check', data=login_data)
